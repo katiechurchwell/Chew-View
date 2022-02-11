40fadb4e964fbc56ea4e57dc7dd5457f5d418d79
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-02-10 18:03:45 -0800

    less movie info, button styles

--- assets/js/restaurant-api.js
+++ assets/js/restaurant-api.js
@@ -1,37 +1,3 @@
-//ON ANY BUTTON CLICK, UNHIDE RESULTS
-var resultsContainer = document.querySelector("#results-container");
-document.querySelectorAll(".btn").forEach(button => {
-  button.addEventListener("click", event => {
-    resultsContainer.classList.remove("hide");
-  })
-})
-
-// CHECKS LOCAL STORAGE FOR ZIP CODE SEARCH HISTORY & AUTO-POPULATES ALL EXISTING ZIP CODES AS BUTTONS
-var zipSearchContainerEl = document.querySelector("#zip-list");
-var localStorageGetZipCodes = "zip-code-list";
-var zipCodeArray;
-if (localStorage.getItem(localStorageGetZipCodes)) {
-  zipCodeArray =
-    JSON.parse(localStorage.getItem(localStorageGetZipCodes)) || [];
-  zipCodeArray.forEach((element) => {
-    var zipEl = document.createElement("li");
-    zipEl.classList = "btn zip-btn zip-btn:hover col-lg-3 col-md-3 col-sm-12";
-    zipEl.textContent = element;
-    zipEl.addEventListener("click", function (event) {
-      var unhideFoodChoices = document.getElementById("restaurant-results-container");
-          unhideFoodChoices.classList.remove("hide");
-      generateGeocode(event.target.textContent);
-    });
-    zipSearchContainerEl.appendChild(zipEl);
-  });
-  var unhideClearButton = document.getElementById("clear");
-      unhideClearButton.classList.remove("hide");
-  var unhideZipHistoryHeader = document.querySelector("#zip-search-label");
-      unhideZipHistoryHeader.classList.remove("hide");
-} else {
-  zipCodeArray = [];
-}
-
 // RESTAURANT API CALL
 var apiKey = "8f375f78cfmshc8b558ca44d4980p13ed1ejsn240d1b3459f9";
 var restaurantEl = document.querySelector("#restaurant"); //results container
@@ -53,45 +19,6 @@ searchBtn.addEventListener("click", function () {
     zipcode.value = "";
   }
 });
-
-// DISPLAY SEARCHED ZIP CODES AS BUTTONS WITHOUT REPEATING ZIP CODES ALREADY SEARCHED
-var displayZips = function(zipcode) {
-    var unhideClearButton = document.getElementById("clear");
-        unhideClearButton.classList.remove("hide");
-    var unhideZipHistoryHeader = document.querySelector("#zip-search-label");
-        unhideZipHistoryHeader.classList.remove("hide");
-  let inArray = false;
-  for (let i = 0; i < zipCodeArray.length; i++) {
-    if (zipCodeArray[i] === zipcode.value) {
-      inArray = true;
-    }
-  }
-  if (!inArray) {
-    zipCodeArray.push(zipcode.value);
-    var zipEl = document.createElement("li");
-    zipEl.classList = "btn zip-btn zip-btn:hover col-lg-3 col-md-3 col-sm-12";
-    zipEl.textContent = zipcode.value;
-    zipEl.addEventListener("click", function (event) {
-      generateGeocode(event.target.textContent);
-    });
-    zipSearchContainerEl.appendChild(zipEl);
-    localStorage.setItem(localStorageGetZipCodes, JSON.stringify(zipCodeArray));
-    console.log(zipEl);
-    console.log(zipCodeArray);
-  }
-};
-
-// 'CLEAR SEARCH HISTORY' BUTTON FUNCTIONS
-var clearSearch = document.querySelector("#clear");
-
-var clearHistory = function() {
-  var hideClearButton = document.getElementById("clear");
-  hideClearButton.classList.add("hide");
-  localStorage.clear();
-  document.location.reload(true);
-};
-
-clearSearch.addEventListener("click", clearHistory);
 
 // GEOCODING API CALL FOR ZIP CODE INFORMATION
 generateGeocode = function (zipcode) {
