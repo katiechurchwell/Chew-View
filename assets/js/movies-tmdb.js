e1b1babbbdf0954dc2f5dd153df6c35803d45ac6
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-02 13:02:08 -0800

    genre button generation based on popular movies

--- assets/js/movies-tmdb.js
+++ assets/js/movies-tmdb.js
@@ -1,8 +1,8 @@
 var resultsContainer = document.querySelector("#results-container"); //results container
-var movieSectionCon = document.querySelector("#movie"); //movie container
+var movieContainer = document.querySelector("#movie"); //movie container
 var movieSearchInputEl = document.querySelector("#movie-search-input"); //movie input
 var searchBool = false;
-var apiKey = "2e30e0c5c0a8e70c539fdbad775c4919";
+const apiKey = "2e30e0c5c0a8e70c539fdbad775c4919";
 
 // DISPLAYS MOVIE CARDS ON HOMEPAGE
 // image = https://image.tmdb.org/t/p/original/ + backdrop_path
@@ -17,11 +17,18 @@ var displayMovies = function (movieData) {
         const genreMatches = allGenresIds.filter((element) =>
           choiceGenres.includes(element)
         );
-        console.log("genreMatches", genreMatches);
-        console.log("genreData", genreData);
-        
-        
-        console.log("matchName", matchName)
+
+        for (i = 0; i < genreData.genres.length; i++) {
+          if (genreMatches.includes(genreData.genres[i].id)) {
+            var genreBtn = document.createElement("button");
+            genreBtn.textContent = genreData.genres[i].name
+            genreBtn.setAttribute(
+              "class",
+              "btn btn-outline-secondary m-1 w-50 movieGenre-btn"
+            );
+            movieContainer.appendChild(genreBtn);
+          }
+        }
       });
     } else {
       console.log("response is not ok");
@@ -45,7 +52,7 @@ var getMovieData = function () {
     }
   }
 
-  removeChildren(movieSectionCon);
+  removeChildren(movieContainer);
 
   fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
@@ -93,7 +100,7 @@ movieClickHandler = function (event) {
   //   }
 };
 
-movieSectionCon.addEventListener("click", movieClickHandler);
+movieContainer.addEventListener("click", movieClickHandler);
 
 var submitBtn = document.querySelector("#submit"); //zipcode search button
 submitBtn.addEventListener("click", movieClickHandler);
