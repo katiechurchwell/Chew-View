c4b38400d887a9c3603f41d5d644200e7d82e891
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-08-30 06:57:23 -0700

    movie genre key

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,17 +3,18 @@ import { useEffect, useState } from "react";
 
 function Movie() {
   const [loading, setLoading] = useState();
-  const [movieGenres, setMovieGenres] = useState();
+  const [movies, setMovies] = useState();
+  const movieApiKey = "2e30e0c5c0a8e70c539fdbad775c4919";
 
   //DISPLAY MOVIE GENRES
   useEffect(() => {
     setLoading(true);
     fetch(
-      `https://api.themoviedb.org/3/genre/movie/list?api_key=2e30e0c5c0a8e70c539fdbad775c4919&language=en-US`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${movieApiKey}&language=en-US&page=1`
     ).then(function (response) {
       if (response.ok) {
-        response.json().then(function (genreData) {
-          setMovieGenres(genreData);
+        response.json().then(function (movieData) {
+          setMovies(movieData);
           setLoading(false);
         });
       } else {
@@ -22,7 +23,7 @@ function Movie() {
     });
   }, []);
 
-  console.log(movieGenres);
+  console.log(movies);
 
   return (
     <ListGroup>
